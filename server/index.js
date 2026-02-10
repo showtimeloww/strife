6cff18d38ff739c3f14b362545dc28a099794fe2
loganoo <showtimeloww@gmail.com>
2026-02-10 18:59:01 -0500

    add health route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
 
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
+
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
